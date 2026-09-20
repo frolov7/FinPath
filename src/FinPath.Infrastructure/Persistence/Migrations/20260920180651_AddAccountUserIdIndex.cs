1a388b0efa3f810e65d2169b3b20e346aa14b27f
Yevgeniy Frolov <gimnast.201180@gmail.com>
2026-09-20 21:08:40 +0300

    FP-5 Добавлен индекс по идентификатору владельца счета

---
+++ src/FinPath.Infrastructure/Persistence/Migrations/20260920180651_AddAccountUserIdIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinPath.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountUserIdIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_accounts_user_id",
+                table: "accounts",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_accounts_user_id",
+                table: "accounts");
+        }
+    }
+}
